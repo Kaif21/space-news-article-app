eb0009b910b4aa8afc20945a7e5491a27bbdfd85
kaif21 <kaif.bd21@gmail.com>
2024-03-31 02:28:10 +0600

    finished the top section ver - 2.0

--- script.js
+++ script.js
@@ -20,23 +20,15 @@ async function fetchFirstDataFromApi() {
   
     const firstItem = firstResultsDataFromTheApi[0];
     const firstDataCard = `
-    <a href="${firstItem.url}">
-    <div class="relative rounded-md hover-img h-96 overflow-hidden border hover:scale-y-110 transition duration-500 cursor-pointer">
-    <img class="max-w-full w-full mx-auto h-full object-cover " src="${firstItem.image_url}" alt="${firstItem.image_url}">
-  <div class="absolute px-5 pt-8 pb-5 bottom-0 w-full h-full">
-    <div class="absolute bottom-5 left-5"> <!-- Position the div at the bottom-left -->
-        <h2 class="text-3xl font-bold capitalize mb-3 text-white">${firstItem.title}</h2>
 
-      <p class="text-white">${firstItem.summary}</p>
-      <div class="pt-2">
-      <div class="">
-        <div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${firstItem.news_site}
+    <img src="${firstItem.image_url}" class="h-full w-full object-cover brightness-75" alt="...">
+    <div class="absolute inset-0 flex flex-col justify-end">
+      <div class="m-4">
+        <h2 class="text-white text-3xl font-bold mb-2">${firstItem.title}</h2>
+        <p class="text-white text-lg">${firstItem.summary}</p>
+        <div class="text-white"><div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${firstItem.news_site}</div>
       </div>
     </div>
-    </div>
-  </div>
-</div>
-</a>
     `;
   
     FirstDataCardCLass.innerHTML = firstDataCard;
@@ -59,7 +51,8 @@ async function fetchData() {
 }
 
 async function renderData() {
-    const cardCLass = document.querySelector('.cardCLass');
+    const cardClass = document.querySelector('.cardClass');
+    console.log(cardClass)
     const resultsFromTheApi = await fetchData();
 
     if (!resultsFromTheApi || !Array.isArray(resultsFromTheApi)) {
@@ -72,24 +65,20 @@ async function renderData() {
 
     apiDataResponseStartFrom2ndItem.forEach(item => {
         const cardTemplate = `
-        <a href="${item.url}" target="_blank">
-        <div class="relative h-48 w-full  hover:scale-90 transition duration-500 cursor-pointer object-cover">
-        <img src="${item.image_url}" alt="${item.title}" class="absolute rounded-md h-full w-full object-cover ">
-        <div class="absolute bottom-0 left-0 p-2 rounded-md">
-          <div class="text-white">
-            <h2 class="text-sm font-bold">${item.title}</h2>
-            <div class="pt-2">
-                        <div class=""><div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${item.news_site}</div>
-                      </div>
+        <div class="relative h-48 border border-slate-900 rounded-lg duration-500 transition-all hover:scale-90">
+        <img src="${item.image_url}" class="h-full w-full object-cover brightness-50 " alt="${item.title}">
+        <div class="absolute inset-0 flex ">
+          <div class="m-4 flex flex-col justify-end">
+            <h2 class="text-white text-base font-bold mb-1">${item.title}</h2>
+            <div class="text-white"><div class="inline-block h-3 border-l-2 border-red-600 mr-2"></div>${item.news_site}</div>
           </div>
         </div>
       </div>
-      </a>
         `;
         cardHTML += cardTemplate;
     });
 
-    cardCLass.innerHTML = cardHTML;
+    cardClass.innerHTML = cardHTML;
 }
 
 renderData();
